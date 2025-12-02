2f1389ccb61b82789df44c2c079e431c5548b0aa
MorLz05 <josueismorl16@gmail.com>
2025-12-01 22:30:53 -0600

    Configurar Program.cs

--- CRUDCORE/Program.cs
+++ CRUDCORE/Program.cs
@@ -2,7 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -18,7 +18,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=Mantenedor}/{action=Listar}/{id?}")
     .WithStaticAssets();
 
 
